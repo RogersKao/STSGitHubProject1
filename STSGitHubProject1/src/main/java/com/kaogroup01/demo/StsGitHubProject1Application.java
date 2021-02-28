7db5ef52a96d6f1cebfd0623769a311bb742fdce
K54088 <rogers.kao@gmail.com>
2021-02-28 00:32:52 -0500

    First Commit.Kao-2021-02-27 9:35PM

---
+++ STSGitHubProject1/src/main/java/com/kaogroup01/demo/StsGitHubProject1Application.java
@@ -0,0 +1,13 @@
+package com.kaogroup01.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGitHubProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGitHubProject1Application.class, args);
+	}
+
+}
---
+++ STSGitHubProject1/src/test/java/com/kaogroup01/demo/StsGitHubProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaogroup01.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsGitHubProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
